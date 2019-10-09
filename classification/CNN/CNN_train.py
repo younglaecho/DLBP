c8309aa547451496a0443ee55dab3bf3b8e1bc6a
younglaecho <48522498+younglaecho@users.noreply.github.com>
2019-10-10 04:36:39 +0900

    Update and rename CNN_train to CNN_train.py

--- classification/CNN/CNN_train.py
+++ classification/CNN/CNN_train.py
@@ -31,38 +31,42 @@ if __name__ == '__main__':
     dataset = MNIST(root='./datasets', train=True, transform=ToTensor(), download=True) # ToTensor : 0~1 값으로 만들어줌
     data_loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=0) # DataLoader(data, 배치사이즈, 셔플, 스레드)
 
-    model = CNN()
+    model = CNN() # CNN이라는 클래스의 인스턴스를 생성, CNN : 딥러닝 모델의 구조(층)를 정의
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss() # Loss 정의
 
     optim = torch.optim.Adam(model.parameters(), lr=0.001)  # parameters : 웨이트들의 값을 돌려줌
         # optim : 웨이트들의 값을 수정해 줌.
         # weight_new = weight_old - weight_gradient * lr
-    if os.path.isfile("./weight_dict.pt"):
-        model_dict = torch.load('./weight_dict.pt')['model_weight']
-        model.load_state_dict(model_dict)
-        adam_dict = torch.load('./weight_dict.pt')['adam_weight']
-        optim.load_state_dict(adam_dict)
+   # if os.path.isfile("./weight_dict.pt"):
+   #    model_dict = torch.load('./weight_dict.pt')['model_weight']
+   #    model.load_state_dict(model_dict)
+   #    adam_dict = torch.load('./weight_dict.pt')['adam_weight']
+   #    optim.load_state_dict(adam_dict)
+
+    # 위 주석처리된 부분은 이미 만들어진 모델의 웨이트가 있다면 불러오는 구문..
 
     list_loss = []
     list_acc = []
-    for epoch in range(1):
+
+    for epoch in range(1): # 1회 훈련
         for input, label in tqdm(data_loader):
             # label 32
             output = model(input) # 32*10
             loss = criterion(output, label) # 1
 
-            optim.zero_grad()
+            optim.zero_grad() # 이전 단계에서 사용한 웨이트 초기화
             loss.backward()
             optim.step()
+            # 웨이트를 적용시킴.
             list_loss.append(loss.detach().item()) # .detach : 파이토치에서 웨이트들과의 관계를 끊음 .item : 파이토치 실수->파이썬
 
-            n_correct_answers = torch.sum(torch.eq(torch.argmax(output, dim=1), label)).item()
-            print("Accuracy:", n_correct_answers / 32 *100)
-            list_acc.append(n_correct_answers / 32 *100)
+            n_correct_answers = torch.sum(torch.eq(torch.argmax(output, dim=1), label)).item() # 정확도 평가
+            print("Accuracy:", n_correct_answers / 32 *100) # 정확도 평가
+            list_acc.append(n_correct_answers / 32 *100) # 정확도 평가
 
-    weight_dict = {'model_weight': model.state_dict(), 'adam_weight': optim.state_dict()}
-    torch.save(weight_dict, "./weight_dict.pt")
+    weight_dict = {'model_weight': model.state_dict(), 'adam_weight': optim.state_dict()}  # 파이썬의 딕셔너리 형태로 웨이트를 저장
+    torch.save(weight_dict, "./weight_dict.pt") # 웨이트 저장
     #torch.save(model.state_dict(), "./CNN_model.pt") # 파이썬의 딕셔너리 형태로 웨이트를 저장
     #torch.save(optim, "./adam.pt")
 
